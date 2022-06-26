1043689ec1118d2d0342873f3c5cd3c36e8b17be
bistimulus <bistimulus0@gmail.com>
2022-06-26 14:15:11 +0530

    Added image and separate modules

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    '''This class will store all the settings for the alien invasion'''
+
+    def __init__(self):
+        '''Initialize the game's settings '''
+        #Screen settings
+        self.screen_width = 1370
+        self.screen_height = 700
+        self.bg_color = (230, 230, 230)
