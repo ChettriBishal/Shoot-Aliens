4056ed599d86d621bde1d2615a2fedcc9381b28d
bistimulus <bistimulus0@gmail.com>
2022-07-03 14:47:13 +0530

    Mouse and P to start game

--- alien_invasion.py
+++ alien_invasion.py
@@ -66,7 +66,11 @@ class AlienInvasion:
 
     def _check_play_button(self,mouse_pos):
         """Start a new game when the player clicks Play"""
-        if self.play_button.rect.collidepoint(mouse_pos):
+        button_clicked =  self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.stats.game_active:
+            self._start_game()
+
+    def _start_game(self):
             #reset the game statistics 
             self.stats.reset_stats()
             self.stats.game_active = True
@@ -79,6 +83,10 @@ class AlienInvasion:
             self._create_fleet()
             self.ship.center_ship()
 
+            #Hide the mouse cursor
+            pygame.mouse.set_visible(False)
+
+
     def _check_keydown_events(self,event): #helper method
         """Respont to keypresses """
         if event.key == pygame.K_RIGHT: # -->
@@ -89,6 +97,8 @@ class AlienInvasion:
             sys.exit()
         elif event.key == pygame.K_SPACE:
             self._fire_bullet()
+        elif event.key == pygame.K_p:
+            self._start_game()
 
     def _check_keyup_events(self,event): #helper method
         if event.key == pygame.K_RIGHT:
@@ -210,6 +220,7 @@ class AlienInvasion:
             sleep(0.5)
         else:
             self.stats.game_active = False
+            pygame.mouse.set_visible(True)
 
 
     def _check_fleet_edges(self):
