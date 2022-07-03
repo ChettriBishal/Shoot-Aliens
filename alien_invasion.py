62ed539a53f864caaffc1b559d9f24536f80dd41
bistimulus <bistimulus0@gmail.com>
2022-07-03 15:39:44 +0530

    Added Scoring

--- alien_invasion.py
+++ alien_invasion.py
@@ -9,6 +9,7 @@ from bullet import Bullet
 from alien import Alien
 from game_stats import GameStats
 from button import Button
+from scoreboard import Scoreboard
 
 class AlienInvasion:
     '''Class to manage game assets and behavior'''
@@ -28,7 +29,9 @@ class AlienInvasion:
         pygame.display.set_caption("Shoot Aliens")
 
         #Create an instance to store game statistics
+        #and create a scoreboard
         self.stats = GameStats(self)
+        self.sb = Scoreboard(self)
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
@@ -71,9 +74,12 @@ class AlienInvasion:
             self._start_game()
 
     def _start_game(self):
+            #reset the game settings
+            self.settings.initialize_dynamic_settings()
             #reset the game statistics 
             self.stats.reset_stats()
             self.stats.game_active = True
+            self.sb.prep_score() # 0 score start
 
             #Get rid of any remaining aliens and bullets
             self.aliens.empty()
@@ -126,10 +132,16 @@ class AlienInvasion:
         #If so, get rid of bullet and the alien
         collisions = pygame.sprite.groupcollide(self.bullets,self.aliens, True, True)
 
+        if collisions:
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
+            self.sb.prep_score()
+
         if not self.aliens:
             #Destroying existing bullets and create a new fleet
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
 
 
     def _fire_bullet(self):
@@ -149,6 +161,10 @@ class AlienInvasion:
             bullet.draw_bullet()
 
         self.aliens.draw(self.screen)
+
+        #Draw the score information
+        self.sb.show_score()
+
 
         #Draw the play button if the game is inactive
         if not self.stats.game_active:
